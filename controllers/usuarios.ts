98e5690eb1efa126fe029d2818b18108b2e04ff5
victor toledo <querynetworking@gmail.com>
2023-05-19 20:45:11 -0300

    configuracion de controladores

---
+++ controllers/usuarios.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express"
+
+export const getUsuarios = ( req: Request, res: Response ) => {
+    res.json({
+        msg: 'getUsuarios'
+    })
+} 
+
+export const getUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'getUsuario',
+        id
+    })
+} 
+
+export const postUsuario = ( req: Request, res: Response ) => {
+
+    const { body } = req;
+
+    res.json({
+        msg: 'postUsuario',
+        body
+    })
+} 
+
+export const putUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+    const { body } = req;
+
+    res.json({
+        msg: 'postUsuario',
+        body
+    })
+} 
+
+export const deleteUsuario = ( req: Request, res: Response ) => {
+
+    const { id } = req.params;
+
+    res.json({
+        msg: 'deleteUsuario',
+        id
+    })
+} 
--- models/server.ts
+++ models/server.ts
@@ -12,7 +12,7 @@ class Server {
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en puerto' + this.port);
+            console.log('Servidor corriendo en puerto ' + this.port);
         })
     }
 }
