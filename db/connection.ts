efbf4c411cf2206b553f76fc7385bf2a77b8b038
Victor Hugo Toledo <121471152+vhtoledo@users.noreply.github.com>
2023-05-21 21:37:01 -0300

    crud basico

--- db/connection.ts
+++ db/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('sutrappa', 'root', '', {
+const db = new Sequelize('midb', 'usuario', '', {
     host: 'localhost',
     dialect: 'mysql',
     // logging: false,
